5ce6266f868ad7fa700debe9c3f212158504f8ba
Yarkeev Denis <d.yarkeev@corp.mail.ru>
2016-04-14 14:49:47 +0300

    fix htmlentities helpers

--- src/lib/handlebars-helpers/htmlentities.js
+++ src/lib/handlebars-helpers/htmlentities.js
@@ -1,32 +1,30 @@
 module.exports = function (Handlebars) {
     'use strict';
 
-    return function (Handlebars) {
-        Handlebars.registerHelper('htmlEntityEncode', function (str) {
-            return String(str)
-                .replace(/&/g, '&amp;')
-                .replace(/"/g, '&quot;')
-                .replace(/'/g, '&#39;')
-                .replace(/</g, '&lt;')
-                .replace(/>/g, '&gt;')
-                .replace(/#/g, '&#35;');
-        });
+    Handlebars.registerHelper('htmlEntityEncode', function (str) {
+        return String(str)
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/#/g, '&#35;');
+    });
 
-        Handlebars.registerHelper('htmlEntityDecode', function (str) {
-            return String(str)
-                .replace(/&amp;/g, '&')
-                .replace(/&quot;/g, '\"')
-                .replace(/&#x27;/g, '\'')
-                .replace(/&#037;/g, '%')
-                .replace(/&#39;/g, '\'')
-                .replace(/&#039;/g, '\'')
-                .replace(/&#92;/g, '\\')
-                .replace(/&#092;/g, '\\')
-                .replace(/&#x3D;/g, '=')
-                .replace(/&#x3d;/g, '=')
-                .replace(/&lt;/g, '<')
-                .replace(/&gt;/g, '>')
-                .replace(/&#35;/g, '#');
-        });
-    };
+    Handlebars.registerHelper('htmlEntityDecode', function (str) {
+        return String(str)
+            .replace(/&amp;/g, '&')
+            .replace(/&quot;/g, '\"')
+            .replace(/&#x27;/g, '\'')
+            .replace(/&#037;/g, '%')
+            .replace(/&#39;/g, '\'')
+            .replace(/&#039;/g, '\'')
+            .replace(/&#92;/g, '\\')
+            .replace(/&#092;/g, '\\')
+            .replace(/&#x3D;/g, '=')
+            .replace(/&#x3d;/g, '=')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
+            .replace(/&#35;/g, '#');
+    });
 };
