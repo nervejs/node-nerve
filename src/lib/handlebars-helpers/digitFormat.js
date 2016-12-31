97340dc8f4d3f3e753ba4369b24d380654adabb1
Yarkeev Denis <d.yarkeev@corp.mail.ru>
2016-12-31 20:31:18 +0300

    fix digitFormat helpers

--- src/lib/handlebars-helpers/digitFormat.js
+++ src/lib/handlebars-helpers/digitFormat.js
@@ -2,6 +2,10 @@ module.exports = function (Handlebars) {
     'use strict';
 
     Handlebars.registerHelper('digitFormat', function (number) {
-        return number.toString().replace(/(\s)+/g, '').replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ');
+        number = number || '';
+
+        return number.toString()
+            .replace(/(\s)+/g, '')
+            .replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ');
     });
 };
