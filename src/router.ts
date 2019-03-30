414ebc1bc8fcd398f7ae81573d27d1f55bbdfb9f
--get-all <d.yarkeev@corp.mail.ru>
2019-03-30 21:19:01 +0300

    add routes of http post requests

--- src/router.ts
+++ src/router.ts
@@ -3,9 +3,9 @@
 import _ = require('lodash');
 import NerveObject = require('./object');
 import path = require('path');
+import { Request, Response } from 'express';
 
-import {NerveApp} from './app';
-import {ClientRequest, ServerResponse} from "http";
+import { NerveApp } from './app';
 
 class Router extends NerveObject {
 
@@ -19,26 +19,37 @@ class Router extends NerveObject {
         this.routes = {};
     }
 
+    initModule(request: Request, response: Response, options: any) {
+        const Module = require(path.resolve(process.cwd(), options.page));
+
+        return new Module(this.app, {
+            request: request,
+            response: response
+        });
+    }
+
     get(url: string, options: any) {
         if (_.isString(options)) {
             options = {
-                page: options
+                page: options,
+                methods: ['get']
             };
         }
 
         this.routes[url] = options;
 
-        this.app.getServer().get(url, (request, response, next) => {
-            const Module = require(path.resolve(process.cwd(), options.page));
+        const server = this.app.getServer();
 
-            return new Module(this.app, {
-                request: request,
-                response: response
-            });
-        });
+        if (!options.method || options.methods.indexOf('get') !== -1) {
+            server.get(url, (request, response) => this.initModule(request, response, options));
+        }
+
+        if (Array.isArray(options.methods) && options.methods.indexOf('post') !== -1) {
+            server.post(url, (request, response) => this.initModule(request, response, options));
+        }
     }
 
-    go(url: string, request: any, response: ServerResponse) {
+    go(url: string, request: any, response: Response) {
         let server: any = this.app.getServer();
 
         request.url = url;
