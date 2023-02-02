404ecdfa9a501777cdb7997ec8becc1e61ae7ffe
n.lebedev <nikita.lebedev@corp.mail.ru>
2023-02-02 12:44:24 +0300

    feat: save render result to page object for further usage

--- src/page.ts
+++ src/page.ts
@@ -39,6 +39,7 @@ class NervePage extends NerveModule {
     protected activeUser: ActiveUser;
 
     protected storeState: any = {};
+    protected renderResult: any = {};
 
     protected isFirstContentRender = false;
 
@@ -409,6 +410,7 @@ class NervePage extends NerveModule {
                 const renderResult = await result;
 
                 html = renderResult.html;
+                this.renderResult = renderResult;
                 this.storeState = renderResult.store;
             } else {
                 this.debug('Template type unknown - returning empty string');
